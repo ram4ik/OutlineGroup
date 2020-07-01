23117a82019a66610b705701ebe064db1cc2cda0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-01 09:37:25 +0300

    Initial Commit

---
+++ OutlineGroup/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  OutlineGroup
+//
+//  Created by ramil on 01.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OutlineGroup/OutlineGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineGroupApp.swift
+//  OutlineGroup
+//
+//  Created by ramil on 01.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct OutlineGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
