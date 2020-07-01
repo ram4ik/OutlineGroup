6b15676901966e06595cef5bae85242f96cafa2c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-07-01 09:47:59 +0300

    init

--- OutlineGroup/ContentView.swift
+++ OutlineGroup/ContentView.swift
@@ -7,9 +7,39 @@
 
 import SwiftUI
 
+struct ListItem: Identifiable {
+    let id: UUID = UUID()
+    let name: String
+    var items: [ListItem]?
+}
+
+extension ListItem {
+    
+    static func all() -> [ListItem] {
+        
+        let animals = [ListItem(name: "🐝"), ListItem(name: "🐛")]
+        let fruits = [ListItem(name: "🌽"), ListItem(name: "🥐"), ListItem(name: "🍇")]
+        let cars = [ListItem(name: "🚒"), ListItem(name: "🚜"), ListItem(name: "🚑")]
+        
+        return [
+            ListItem(name: "Animals", items: animals),
+            ListItem(name: "Fruits", items: fruits),
+            ListItem(name: "Cars", items: cars)
+        ]
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        List{
+            ForEach(ListItem.all()) { category in
+                Section(header: EmptyView()) {
+                    OutlineGroup(category, children: \.items) { item in
+                        Text(item.name)
+                    }
+                }
+            }
+        }
     }
 }
 
